d80ddb2221d7e1b95027c08be22dd415efbd9f86
GG79 <midlifegamingcrisis@gmail.com>
2022-10-16 06:28:03 -0700

    Added door locks

---
+++ configs/pillbox.lua
@@ -0,0 +1,57 @@
+
+
+-- chief created by pamel
+Config.DoorList['pillbox-chief'] = {
+    authorizedJobs = { ['ambulance'] = 0 },
+    distance = 2,
+    doorType = 'door',
+    objYaw = 340.00003051758,
+    fixText = false,
+    objCoords = vec3(336.866486, -592.578796, 43.433910),
+    locked = true,
+    objName = 854291622,
+    doorLabel = 'Chief's Office',
+    doorRate = 1.0,
+}
+
+-- laboratory created by pamel
+Config.DoorList['pillbox-laboratory'] = {
+    authorizedJobs = { ['ambulance'] = 0 },
+    distance = 2,
+    doorType = 'door',
+    objYaw = 340.00003051758,
+    fixText = false,
+    objCoords = vec3(307.118195, -569.568970, 43.433910),
+    locked = true,
+    objName = 854291622,
+    doorLabel = 'Laboratory',
+    doorRate = 1.0,
+}
+
+-- reception created by pamel
+Config.DoorList['pillbox-reception'] = {
+    authorizedJobs = { ['ambulance'] = 0 },
+    distance = 2,
+    doorType = 'door',
+    objYaw = 249.98275756836,
+    fixText = false,
+    objCoords = vec3(313.480072, -595.458313, 43.433910),
+    locked = true,
+    objName = 854291622,
+    doorLabel = 'Reception',
+    doorRate = 1.0,
+}
+
+-- staff-room created by pamel
+Config.DoorList['pillbox-staff-room'] = {
+    authorizedJobs = { ['ambulance'] = 0 },
+    distance = 2,
+    doorType = 'door',
+    objYaw = 160.00003051758,
+    fixText = false,
+    objCoords = vec3(309.133728, -597.751465, 43.433910),
+    locked = true,
+    objName = 854291622,
+    doorLabel = 'Staff Room',
+    doorRate = 1.0,
+}
