8df39e5f2616774422dd4ee635ebec8c1d88ca8f
GG79 <midlifegamingcrisis@gmail.com>
2023-02-23 12:41:16 +0200

    added door locks for la fuente

---
+++ configs/lafuente.lua
@@ -0,0 +1,46 @@
+
+
+-- johndoor created by GamerGirlie79
+Config.DoorList['lafuente-johndoor'] = {
+    doorRate = 1.0,
+    pickable = true,
+    locked = true,
+    objName = 736699661,
+    doorLabel = 'John's Room',
+    fixText = false,
+    authorizedCitizenIDs = { ['XEG37128'] = true },
+    doorType = 'door',
+    distance = 2,
+    objCoords = vec3(1401.060303, 1158.948853, 117.641243),
+    objYaw = 89.999977111816,
+}
+
+-- nickdoor created by GamerGirlie79
+Config.DoorList['lafuente-nickdoor'] = {
+    doorRate = 1.0,
+    pickable = true,
+    locked = true,
+    objName = 736699661,
+    doorLabel = 'Nick's Room',
+    fixText = false,
+    authorizedCitizenIDs = { ['OFN28516'] = true },
+    doorType = 'door',
+    distance = 2,
+    objCoords = vec3(1400.996216, 1158.950562, 114.483452),
+    objYaw = 89.999961853027,
+}
+
+-- cruisedoor created by GamerGirlie79
+Config.DoorList['lafuente-cruisedoor'] = {
+    doorRate = 1.0,
+    pickable = true,
+    locked = true,
+    objName = 736699661,
+    doorLabel = 'Cruise's Room',
+    fixText = false,
+    authorizedCitizenIDs = { ['FJR80957'] = true },
+    doorType = 'door',
+    distance = 2,
+    objCoords = vec3(1401.014526, 1144.449951, 117.637360),
+    objYaw = 89.999977111816,
+}
---
+++ configs/prison.lua
@@ -0,0 +1,449 @@
+
+
+-- prison created by GamerGirlie79
+Config.DoorList['prison-prison'] = {
+    objYaw = 179.99987792969,
+    locked = true,
+    fixText = false,
+    objCoords = vec3(1798.090088, 2591.687256, 46.417839),
+    objName = -1156020871,
+    hideLabel = true,
+    authorizedJobs = { ['police'] = 0 },
+    distance = 2,
+    pickable = true,
+    doorType = 'door',
+    doorRate = 1.0,
+    doorLabel = 'Gate 1',
+}
+
+-- prison2 created by GamerGirlie79
+Config.DoorList['prison-prison2'] = {
+    objYaw = 179.99987792969,
+    locked = true,
+    fixText = false,
+    objCoords = vec3(1797.760864, 2596.564941, 46.387306),
+    objName = -1156020871,
+    hideLabel = true,
+    authorizedJobs = { ['police'] = 0 },
+    distance = 2,
+    pickable = true,
+    doorType = 'door',
+    doorRate = 1.0,
+    doorLabel = 'Gate 2',
+}
+
+-- prison3 created by GamerGirlie79
+Config.DoorList['prison-prison3'] = {
+    objYaw = 270.00003051758,
+    locked = true,
+    fixText = false,
+    objCoords = vec3(1819.074341, 2594.874512, 46.086990),
+    objName = 1373390714,
+    hideLabel = true,
+    authorizedJobs = { ['police'] = 0 },
+    distance = 2,
+    pickable = true,
+    doorType = 'door',
+    doorRate = 1.0,
+    doorLabel = 'Foyer Back Door',
+}
+
+-- prison4 created by GamerGirlie79
+Config.DoorList['prison-prison4'] = {
+    objYaw = 0.0,
+    locked = true,
+    fixText = false,
+    objCoords = vec3(1837.911865, 2590.256104, 46.197884),
+    objName = 539686410,
+    hideLabel = true,
+    authorizedJobs = { ['police'] = 0 },
+    distance = 2,
+    pickable = true,
+    doorType = 'door',
+    doorRate = 1.0,
+    doorLabel = 'Foyer Gate',
+}
+
+-- prison5 created by GamerGirlie79
+Config.DoorList['prison-prison5'] = {
+    objYaw = 90.0,
+    locked = true,
+    fixText = false,
+    objCoords = vec3(1818.542847, 2604.812500, 44.611004),
+    objName = 741314661,
+    hideLabel = true,
+    authorizedJobs = { ['police'] = 0 },
+    distance = 5,
+    pickable = true,
+    doorType = 'sliding',
+    doorRate = 1.0,
+    doorLabel = 'Vehicle Gate 2',
+}
+
+-- prison6 created by GamerGirlie79
+Config.DoorList['prison-prison6'] = {
+    objYaw = 90.0,
+    locked = true,
+    fixText = false,
+    objCoords = vec3(1844.998413, 2604.812500, 44.639778),
+    objName = 741314661,
+    hideLabel = true,
+    authorizedJobs = { ['police'] = 0 },
+    distance = 5,
+    pickable = true,
+    doorType = 'sliding',
+    doorRate = 1.0,
+    doorLabel = 'Vehicle Gate 1',
+}
+
+-- prison7 created by GamerGirlie79
+Config.DoorList['prison-prison7'] = {
+    objYaw = 179.99998474121,
+    locked = true,
+    fixText = false,
+    objCoords = vec3(1799.608398, 2616.975342, 44.603249),
+    objName = 741314661,
+    hideLabel = true,
+    authorizedJobs = { ['police'] = 0 },
+    distance = 5,
+    pickable = true,
+    doorType = 'sliding',
+    doorRate = 1.0,
+    doorLabel = 'Vehicle Gate 3',
+}
+
+-- prison8 created by GamerGirlie79
+Config.DoorList['prison-prison8'] = {
+    objYaw = 89.999977111816,
+    locked = true,
+    fixText = false,
+    objCoords = vec3(1791.595215, 2551.462158, 45.753204),
+    objName = 1373390714,
+    hideLabel = true,
+    authorizedJobs = { ['police'] = 0 },
+    distance = 2,
+    pickable = true,
+    doorType = 'door',
+    doorRate = 1.0,
+    doorLabel = 'Cells Entrance',
+}
+
+-- prison9 created by GamerGirlie79
+Config.DoorList['prison-prison9'] = {
+    doors = {
+        {objName = 741314661, objYaw = 110.00004577637, objCoords = vec3(1835.284790, 2689.104004, 44.446701)},
+        {objName = 741314661, objYaw = 289.16897583008, objCoords = vec3(1830.134277, 2703.498535, 44.446701)}
+    },
+    locked = true,
+    hideLabel = true,
+    authorizedJobs = { ['police'] = 0 },
+    distance = 10,
+    pickable = true,
+    doorType = 'doublesliding',
+    doorRate = 1.0,
+    doorLabel = 'Double Gate 1',
+}
+
+-- prison10 created by GamerGirlie79
+Config.DoorList['prison-prison10'] = {
+    doors = {
+        {objName = 741314661, objYaw = 160.00001525879, objCoords = vec3(1776.701294, 2747.147949, 44.446690)},
+        {objName = 741314661, objYaw = 339.62002563477, objCoords = vec3(1762.195557, 2752.488770, 44.446690)}
+    },
+    locked = true,
+    hideLabel = true,
+    authorizedJobs = { ['police'] = 0 },
+    distance = 10,
+    pickable = true,
+    doorType = 'doublesliding',
+    doorRate = 1.0,
+    doorLabel = 'Double Gate 2',
+}
+
+-- prison11 created by GamerGirlie79
+Config.DoorList['prison-prison11'] = {
+    doors = {
+        {objName = 741314661, objYaw = 207.17547607422, objCoords = vec3(1662.010986, 2748.702881, 44.446690)},
+        {objName = 741314661, objYaw = 27.17546081543, objCoords = vec3(1648.411377, 2741.668457, 44.446690)}
+    },
+    locked = true,
+    hideLabel = true,
+    authorizedJobs = { ['police'] = 0 },
+    distance = 10,
+    pickable = true,
+    doorType = 'doublesliding',
+    doorRate = 1.0,
+    doorLabel = 'Double Gate 3',
+}
+
+-- prison12 created by GamerGirlie79
+Config.DoorList['prison-prison12'] = {
+    doors = {
+        {objName = 741314661, objYaw = 233.70986938477, objCoords = vec3(1584.652832, 2679.749512, 44.509468)},
+        {objName = 741314661, objYaw = 54.548603057861, objCoords = vec3(1575.719482, 2667.151855, 44.509468)}
+    },
+    locked = true,
+    hideLabel = true,
+    authorizedJobs = { ['police'] = 0 },
+    distance = 10,
+    pickable = true,
+    doorType = 'doublesliding',
+    doorRate = 1.0,
+    doorLabel = 'Double Gate 4',
+}
+
+-- prison13 created by GamerGirlie79
+Config.DoorList['prison-prison13'] = {
+    doors = {
+        {objName = 741314661, objYaw = 267.01473999023, objCoords = vec3(1547.706177, 2591.282227, 44.509468)},
+        {objName = 741314661, objYaw = 87.0146484375, objCoords = vec3(1546.983398, 2576.129639, 44.390327)}
+    },
+    locked = true,
+    hideLabel = true,
+    authorizedJobs = { ['police'] = 0 },
+    distance = 10,
+    pickable = true,
+    doorType = 'doublesliding',
+    doorRate = 1.0,
+    doorLabel = 'Double Gate 5',
+}
+
+-- prison14 created by GamerGirlie79
+Config.DoorList['prison-prison14'] = {
+    doors = {
+        {objName = 741314661, objYaw = 298.04623413086, objCoords = vec3(1550.930420, 2482.743408, 44.395287)},
+        {objName = 741314661, objYaw = 118.04624938965, objCoords = vec3(1558.221191, 2469.349121, 44.395287)}
+    },
+    locked = true,
+    hideLabel = true,
+    authorizedJobs = { ['police'] = 0 },
+    distance = 10,
+    pickable = true,
+    doorType = 'doublesliding',
+    doorRate = 1.0,
+    doorLabel = 'Double Gate 6',
+}
+
+-- prison15 created by GamerGirlie79
+Config.DoorList['prison-prison15'] = {
+    doors = {
+        {objName = 741314661, objYaw = 353.00042724609, objCoords = vec3(1652.983887, 2409.571289, 44.443077)},
+        {objName = 741314661, objYaw = 173.00039672852, objCoords = vec3(1667.669312, 2407.647705, 44.428795)}
+    },
+    locked = true,
+    hideLabel = true,
+    authorizedJobs = { ['police'] = 0 },
+    distance = 10,
+    pickable = true,
+    doorType = 'doublesliding',
+    doorRate = 1.0,
+    doorLabel = 'Double Gate 7',
+}
+
+-- prison16 created by GamerGirlie79
+Config.DoorList['prison-prison16'] = {
+    doors = {
+        {objName = 741314661, objYaw = 26.757732391357, objCoords = vec3(1749.141846, 2419.812012, 44.425171)},
+        {objName = 741314661, objYaw = 206.12844848633, objCoords = vec3(1762.541992, 2426.506836, 44.437866)}
+    },
+    locked = true,
+    hideLabel = true,
+    authorizedJobs = { ['police'] = 0 },
+    distance = 10,
+    pickable = true,
+    doorType = 'doublesliding',
+    doorRate = 1.0,
+    doorLabel = 'Double Gate 8',
+}
+
+-- prison17 created by GamerGirlie79
+Config.DoorList['prison-prison17'] = {
+    doors = {
+        {objName = 741314661, objYaw = 70.905723571777, objCoords = vec3(1808.991577, 2474.544922, 44.480774)},
+        {objName = 741314661, objYaw = 251.97775268555, objCoords = vec3(1813.749268, 2488.906982, 44.463684)}
+    },
+    locked = true,
+    hideLabel = true,
+    authorizedJobs = { ['police'] = 0 },
+    distance = 10,
+    pickable = true,
+    doorType = 'doublesliding',
+    doorRate = 1.0,
+    doorLabel = 'Double Gate 9',
+}
+
+-- prison18 created by GamerGirlie79
+Config.DoorList['prison-prison18'] = {
+    objYaw = 269.50003051758,
+    locked = true,
+    fixText = false,
+    objCoords = vec3(1708.481812, 2564.782471, 46.252220),
+    objName = -1156020871,
+    hideLabel = true,
+    authorizedJobs = { ['police'] = 0 },
+    distance = 10,
+    pickable = true,
+    doorType = 'door',
+    doorRate = 1.0,
+    doorLabel = 'Yard Gate 1',
+}
+
+-- prison19 created by GamerGirlie79
+Config.DoorList['prison-prison19'] = {
+    objYaw = 269.50003051758,
+    locked = true,
+    fixText = false,
+    objCoords = vec3(1681.208374, 2564.782227, 46.252220),
+    objName = -1156020871,
+    hideLabel = true,
+    authorizedJobs = { ['police'] = 0 },
+    distance = 10,
+    pickable = true,
+    doorType = 'door',
+    doorRate = 1.0,
+    doorLabel = 'Yard Gate 2',
+}
+
+-- prison20 created by GamerGirlie79
+Config.DoorList['prison-prison20'] = {
+    objYaw = 134.28955078125,
+    locked = true,
+    fixText = false,
+    objCoords = vec3(1618.330444, 2573.611084, 46.252220),
+    objName = -1156020871,
+    hideLabel = true,
+    authorizedJobs = { ['police'] = 0 },
+    distance = 10,
+    pickable = true,
+    doorType = 'door',
+    doorRate = 1.0,
+    doorLabel = 'Yard Gate 3',
+}
+
+-- prison21 created by GamerGirlie79
+Config.DoorList['prison-prison21'] = {
+    objYaw = 45.500064849854,
+    locked = true,
+    fixText = false,
+    objCoords = vec3(1611.607910, 2538.815430, 44.665760),
+    objName = 729940451,
+    hideLabel = true,
+    authorizedJobs = { ['police'] = 0 },
+    distance = 5,
+    pickable = true,
+    doorType = 'door',
+    doorRate = 1.0,
+    doorLabel = 'Yard Gate 4',
+}
+
+-- prison22 created by GamerGirlie79
+Config.DoorList['prison-prison22'] = {
+    objYaw = 185.64677429199,
+    locked = true,
+    fixText = false,
+    objCoords = vec3(1623.320312, 2519.109375, 46.252220),
+    objName = -1156020871,
+    hideLabel = true,
+    authorizedJobs = { ['police'] = 0 },
+    distance = 5,
+    pickable = true,
+    doorType = 'door',
+    doorRate = 1.0,
+    doorLabel = 'Yard Gate 5',
+}
+
+-- prison23 created by GamerGirlie79
+Config.DoorList['prison-prison23'] = {
+    objYaw = 275.06982421875,
+    locked = true,
+    fixText = false,
+    objCoords = vec3(1653.763306, 2493.576660, 46.252220),
+    objName = -1156020871,
+    hideLabel = true,
+    authorizedJobs = { ['police'] = 0 },
+    distance = 5,
+    pickable = true,
+    doorType = 'door',
+    doorRate = 1.0,
+    doorLabel = 'Yard Gate 6',
+}
+
+-- prison24 created by GamerGirlie79
+Config.DoorList['prison-prison24'] = {
+    objYaw = 224.84396362305,
+    locked = true,
+    fixText = false,
+    objCoords = vec3(1673.032715, 2489.581299, 46.252220),
+    objName = -1156020871,
+    hideLabel = true,
+    authorizedJobs = { ['police'] = 0 },
+    distance = 5,
+    pickable = true,
+    doorType = 'door',
+    doorRate = 1.0,
+    doorLabel = 'Yard Gate 7',
+}
+
+-- prison25 created by GamerGirlie79
+Config.DoorList['prison-prison25'] = {
+    objYaw = 313.96069335938,
+    locked = true,
+    fixText = false,
+    objCoords = vec3(1712.759888, 2489.613037, 46.252220),
+    objName = -1156020871,
+    hideLabel = true,
+    authorizedJobs = { ['police'] = 0 },
+    distance = 5,
+    pickable = true,
+    doorType = 'door',
+    doorRate = 1.0,
+    doorLabel = 'Yard Gate 8',
+}
+
+-- prison26 created by GamerGirlie79
+Config.DoorList['prison-prison26'] = {
+    objYaw = 255.21278381348,
+    locked = true,
+    fixText = false,
+    objCoords = vec3(1727.015625, 2509.423584, 46.062405),
+    objName = -1156020871,
+    hideLabel = true,
+    authorizedJobs = { ['police'] = 0 },
+    distance = 5,
+    pickable = true,
+    doorType = 'door',
+    doorRate = 1.0,
+    doorLabel = 'Yard Gate 9',
+}
+
+-- prison27 created by GamerGirlie79
+Config.DoorList['prison-prison27'] = {
+    objYaw = 344.50881958008,
+    locked = true,
+    fixText = false,
+    objCoords = vec3(1761.397705, 2529.338135, 46.252220),
+    objName = -1156020871,
+    hideLabel = true,
+    authorizedJobs = { ['police'] = 0 },
+    distance = 5,
+    pickable = true,
+    doorType = 'door',
+    doorRate = 1.0,
+    doorLabel = 'Yard Gate 10',
+}
+
+-- prison28 created by GamerGirlie79
+Config.DoorList['prison-prison28'] = {
+    objYaw = 269.50003051758,
+    locked = true,
+    fixText = false,
+    objCoords = vec3(1744.181641, 2562.525391, 46.252220),
+    objName = -1156020871,
+    hideLabel = true,
+    authorizedJobs = { ['police'] = 0 },
+    distance = 5,
+    pickable = true,
+    doorType = 'door',
+    doorRate = 1.0,
+    doorLabel = 'Yard Gate 11',
+}
